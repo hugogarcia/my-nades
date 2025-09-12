5cf7f63b8b63bd58f25654a7534473765c297eb1
Mayron Noriduki <mayron.noriduki@gmail.com>
2025-09-11 21:40:18 -0300

    meu primeiro commit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! Aqui foi em!", name)
+    format!("Meu consagrado, {}! Aqui foi em!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
